2fe7268f95c23ac0813f9b0fc0edb951c856aad1
assignment <bmwthriad2023@gmail.com>
2025-11-14 01:04:40 +0600

    Till Login Screen

---
+++ lib/ui/screens/login_screen.dart
@@ -0,0 +1,190 @@
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:task_manager/data/models/user_model.dart';
+import 'package:task_manager/data/services/api_caller.dart';
+import 'package:task_manager/data/utils/urls.dart';
+import 'package:task_manager/ui/controllers/auth_controller.dart';
+import 'package:task_manager/ui/screens/forgot_password_verify_email_screen.dart';
+import 'package:task_manager/ui/screens/main_nav_bar_holder_screen.dart';
+import 'package:task_manager/ui/screens/sign_up_screen.dart';
+import 'package:task_manager/ui/widgets/centered_progress_indicator.dart';
+import 'package:task_manager/ui/widgets/screen_background.dart';
+import 'package:task_manager/ui/widgets/snack_bar_message.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  static const String name = '/login'; // Global naming for routes
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailTEController = TextEditingController();
+  final TextEditingController _passwordTEController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  bool _loginInProgress = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ScreenBackground(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Form(
+              key: _formKey,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 82),
+                  Text(
+                    'Get Started With',
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  const SizedBox(height: 24),
+                  TextFormField(
+                    controller: _emailTEController,
+                    decoration: InputDecoration(hintText: 'Email'),
+                    validator: (String? value) {
+                      String inputText = value ?? '';
+                      if (EmailValidator.validate(inputText) == false) {
+                        return 'Enter a valid email';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    controller: _passwordTEController,
+                    obscureText: true,
+                    decoration: InputDecoration(hintText: 'Password'),
+                    validator: (String? value) {
+                      if ((value?.length ?? 0) <= 6) {
+                        return 'Password should more than 6 letters';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  Visibility(
+                    visible: _loginInProgress == false,
+                    replacement: CenteredProgressIndicator(),
+                    child: FilledButton(
+                      onPressed: _onTapLoginButton, // This is Login Button
+                      child: Icon(Icons.arrow_circle_right_outlined),
+                    ),
+                  ),
+                  const SizedBox(height: 36),
+                  Center(
+                    child: Column(
+                      children: [
+                        TextButton(
+                          onPressed: _onTapForgotPasswordButton,
+                          child: Text(
+                            'Forgot Password?',
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ),
+                        RichText(
+                          text: TextSpan(
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600,
+                            ),
+                            text: "Don't have an account? ",
+                            children: [
+                              TextSpan(
+                                text: 'Sign up',
+                                style: TextStyle(color: Colors.green),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = _onTapSignUpButton,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _onTapSignUpButton() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SignUpScreen()),
+    );
+  }
+
+  void _onTapForgotPasswordButton() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => ForgotPasswordVerifyEmailScreen(),
+      ),
+    );
+  }
+
+  void _onTapLoginButton() {
+    // Login Button Function
+    if (_formKey.currentState!.validate()) {
+      _login();
+    }
+  }
+
+  // Api Calling for Login Request ================ //
+
+  Future<void> _login() async {
+    // API Called
+    _loginInProgress = true;
+    setState(() {});
+    Map<String, dynamic> requestBody = {
+      "email": _emailTEController.text.trim(),
+      "password": _passwordTEController.text,
+    }; // Prepare Body
+
+    // postRequest is main a ApiResponse type method
+    final ApiResponse response = await ApiCaller.postRequest(
+      url: Urls.loginUrl, // Url in Utils -> This is loginApiURl
+      body: requestBody,
+    );
+    if (response.isSuccess && response.responseData['status'] == 'success') {
+      // Getting data from response
+      UserModel model = UserModel.fromJson(response.responseData['data']);
+      // Getting data from response
+      String token = response.responseData['token'];
+
+      // Saving data by passing in the method
+      await AuthController.saveUserData(model, token);
+
+      // Conclusion is : Before Going to MainNavBarHolder Screen it will save user info
+      await Navigator.pushNamedAndRemoveUntil(
+        context,
+        MainNavBarHolderScreen.name,
+        (predicate) => false,
+      );
+    } else {
+      _loginInProgress = false;
+      setState(() {});
+      showSnackBarMessage(context, response.errorMessage!);
+    }
+  }
+  // Api Calling for Login Request ================ //
+
+  @override
+  void dispose() {
+    _emailTEController.dispose();
+    _passwordTEController.dispose();
+    super.dispose();
+  }
+}
